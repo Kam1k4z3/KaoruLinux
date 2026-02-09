9bdc8967aa1ec5070ab8796fff8fed5775f36062
kamikinsyderrr <andrecamill2155@gmail.com>
2026-02-09 23:35:05 +0100

    users working properly now, fixed suid permissions for busybox, added /etc/profile

--- gen_initramfs.sh
+++ gen_initramfs.sh
@@ -1,4 +1,25 @@
 cd boot/rootfs
+mkdir -p etc
+rm etc/profile
+# touch etc/profile
+# echo "export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >> etc/profile
+cp ../profile etc/profile
+rm etc/passwd
+touch etc/passwd
+echo "root:x:0:0:root:/root:/bin/oksh" >> etc/passwd
+rm etc/group
+tee -a etc/group <<EOF
+root:x:0:
+bin:x:1:
+daemon:x:2:
+sys:x:3:
+wheel:x:10:root
+EOF
+rm etc/shadow
+touch etc/shadow
+echo "root:::0:99999:7:::" >> etc/shadow
+mkdir -p ./root
+chmod 700 ./root
 rm ../init.cpio
 find . | cpio -o -H newc > ../init.cpio
 cp ../init init
